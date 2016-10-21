f3b06e6ad0cc97458556e6b14fbaa5150a48a41f
Andrey Zavgorodniy <azavgorodniy@ptsecurity.com>
2016-10-21 20:35:36 +0300

    modified gru

--- main.py
+++ main.py
@@ -3,15 +3,35 @@
 import sys
 import logging
 
-from python.inputs.simple_sequence_input import SimpleSequenceInput
-from python.models.multilayer_gru import MultilayerGRU
+import tensorflow as tf
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+from src.inputs.sequence_input import TrainSequenceInput, TestSequenceInput
+from src.models.multilayer_gru import MultilayerGRU
+
+logging.basicConfig(stream=sys.stdout, format="", level=logging.INFO)
+
+truncate_at = 10
+batch_size = 50
+
+num_hidden = 7
+num_layers = 2
 
 
 def main():
-    training_data = SimpleSequenceInput(sequence_len=10000000)
-    MultilayerGRU(num_hidden_neurons=10).train(training_data)
+    train_data = TrainSequenceInput(
+        sequence_len=10000000, num_epochs=50,
+        batch_size=batch_size, truncate_at=truncate_at)
+    test_data = TestSequenceInput(
+        sequence_len=1000, truncate_at=truncate_at)
+    with tf.Graph().as_default(), tf.Session() as sess:
+        with tf.variable_scope("model", reuse=None):
+            model = MultilayerGRU(
+                num_hidden_neurons=num_hidden, num_gru_layers=num_layers)
+            model.fit(sess, train_data)
+        with tf.variable_scope("model", reuse=True):
+            eval_model = MultilayerGRU(
+                num_hidden_neurons=num_hidden, num_gru_layers=num_layers)
+            eval_model.predict(sess, test_data)
 
 
 if __name__ == "__main__":
--- python/models/multilayer_gru.py
+++ python/models/multilayer_gru.py
@@ -12,35 +12,55 @@ class MultilayerGRU(object):
     unfolding_depth = None
     h_to_o_scope = "hidden_to_output"
 
+    x = None
+    y = None
     hidden = None
     hidden_shape = None
+    _final_hidden = None
     _initial_hidden = None
+    _logits = None
+    _softmax = None
 
-    def __init__(self, num_hidden_neurons=4, num_lstm_layers=2, eta=0.1):
+    def __init__(self,
+                 eta=0.1,
+                 num_hidden_neurons=4,
+                 num_gru_layers=2,
+                 dropout_prob=1.0):
         """Initializes local and tensorflow structures and dependencies.
 
         Args:
-            num_hidden_neurons: Number of neurons in hodden layer.
             eta: learning rate.
+            num_hidden_neurons: Number of neurons in hodden layer.
+            num_gru_layers: Number of stacked gru layers.
+            dropout_prob: Float between 0 and 1, input keep probability; if it
+                is 1, no input dropout will be added.
         """
         self.eta = eta
-        self.num_gru_layers = num_lstm_layers
+        self.dropout_prob = dropout_prob
+        self.num_gru_layers = num_gru_layers
         self.num_hidden_neurons = num_hidden_neurons
 
-    def train(self, training_data):
-        self.num_classes = training_data.num_classes
-        self.unfolding_depth = training_data.truncate_at
-        self.hidden_shape = [training_data.batch_size, self.num_hidden_neurons]
-        batch_size = training_data.batch_size
+    def fit(self, sess, data):
+        self._setup_common(data)
+        self._train(sess, data)
+
+    def predict(self, sess, data):
+        self._setup_common(data)
+        return self._predict(sess, data)
+
+    def _setup_common(self, data):
+        self.num_classes = data.num_classes
+        self.unfolding_depth = data.truncate_at
+        self.hidden_shape = [data.batch_size, self.num_hidden_neurons]
+        batch_size = data.batch_size
         # A batch (unputs and labels).
-        x = tf.placeholder(tf.int32, [batch_size, self.unfolding_depth])
-        y = tf.placeholder(tf.int32, [batch_size, self.unfolding_depth])
+        self.x = tf.placeholder(tf.int32, [batch_size, self.unfolding_depth])
         # Turn our x placeholder into a list of one-hot tensors. We just need
         # to tell `tf.one_hot` the size of our vocabulary (self.num_classes),
         # and it will generate appropriate one-hot vectors.
         # As `x` has shape (batch_size, truncate_at), `x_one_hot` will have
         # shape (batch_size, truncate_at, num_classes)
-        x_one_hot = tf.one_hot(x, self.num_classes)
+        x_one_hot = tf.one_hot(self.x, self.num_classes)
         # We can't use `x_one_hot` as is. After unpacking it along the
         # `num_classes` axis `rnn_inputs` is a list of length `truncate_at`
         # containing tensors with shape (batch_size, num_classes).
@@ -66,87 +86,20 @@ class MultilayerGRU(object):
         self._initial_hidden = stacked_gru.zero_state(batch_size, tf.float32)
         # After feeding another batch to network we have some final hidden
         # state that should be used as the "initial" (t_{-1}) state when
-        # feeding the next batch. Note that `_final_hidden` here is a
+        # feeding the next batch. Note that `self._final_hidden` here is a
         # tensorflow object that is not evaluated yet.
-        self.hidden, _final_hidden = tf.nn.rnn(
+        self.hidden, self._final_hidden = tf.nn.rnn(
             stacked_gru, rnn_inputs, initial_state=self._initial_hidden)
-        # Initialize weight matrices and biases as tensorflow variables.
-        self._setup_model_params()
-        train_step, _step_loss = self._setup_predictions(y)
-        with tf.Session() as sess:
-            sess.run(tf.initialize_all_variables())
-            for epoch_num, epoch in enumerate(training_data):
-                step_loss = 0.
-                # `final_hidden` is the evaluated final hidden state for
-                # current batch. We tell tensorflow to use it as the
-                # "initial" (t_{-1}) state for the next batch by placing
-                # it in the `feed_dict` kwarg. Note that `self._initial_hidden`
-                # is a tensorflow (unevaluated) tensorflow object that is
-                # used in `self._setup_input_processing`.
-                final_hidden = None
-                for X, Y in epoch:
-                    # `step_loss` is the mean loss for current step. Note that
-                    # we pass `_step_loss` to `sess.run` just because we want
-                    # to print something after each epoch. The only value that
-                    # we really need to evaluate (and reuse) for training is
-                    # `_final_hidden`.
-                    feed_dict = {x: X, y: Y}
-                    # First step already has valid (zero) initial hidden state.
-                    # For further steps we need to use final hidden state for
-                    # current batch as our new initial hidden state.
-                    #
-                    # `is not None` here is not bad style; `final_hidden` is
-                    # going to become an array an will raise a `ValueError` if
-                    # being used as a truth value.
-                    if final_hidden is not None:
-                        feed_dict[self._initial_hidden] = final_hidden
-                    step_loss, final_hidden, _ = sess.run(
-                        [_step_loss, _final_hidden, train_step],
-                        feed_dict=feed_dict)
-                logging.info("Epoch %s, mean loss: %s",
-                             epoch_num, np.mean(step_loss))
-
-    def _setup_model_params(self):
-        """Initializes weight matrices and biases as tensorflow variables.
-
-        Our model has only one hidden layer. We need to initialize
-        input-to-hidden and hidden-to-output weight matrices and bias vectors.
-
-        Because we concatenate (t, current) input and (t-1, previous) hidden
-        state into one vector, there's a single input-to-hidden weight matrix
-        `W` and a single bias vector `b`. Its shape (num_classes + self.h_size,
-        self.h_size) is due to the fact that input and output one-hot vectors
-        have the same size (self.num_classes); obviously `W` treats inputs as
-        row-vectors (not column-vectors).
-        """
         num_classes, num_hidden = self.num_classes, self.num_hidden_neurons
         with tf.variable_scope(self.h_to_o_scope):
-            tf.get_variable('W', [num_hidden, num_classes])
-            tf.get_variable('b', [num_classes],
-                            initializer=tf.constant_initializer(0.0))
+            W = tf.get_variable('W', [num_hidden, num_classes])
+            b = tf.get_variable('b', [num_classes],
+                                initializer=tf.constant_initializer(0.0))
+        self._logits = [tf.matmul(hid, W) + b for hid in self.hidden]
+        # Predictions (not used here directly).
+        self._softmax = [tf.nn.softmax(logit) for logit in self._logits]
 
-    def _get_model_params(self, layer_scope):
-        """Returns model parameters `W` and `b` from a certain layer.
-
-        Args:
-            layer_scope: name of the tensorflow scope containing a layer's
-                params (stored as tensorflow variables).
-        Returns:
-            W: a 2D tensorflow variable representing a weight matrix.
-            b: a 1D tensorflow variable representing a bias vector.
-        Raises:
-            An `Exception` if layer_scope is not self.i_to_h_scope or
-            self.h_to_o_scope (we have a fixed model with one hidden layer).
-
-        """
-        if layer_scope not in [self.h_to_o_scope]:
-            raise Exception("Unknown layer scope %s" % layer_scope)
-        with tf.variable_scope(layer_scope, reuse=True):
-            # Variables are reused, no shapes or initializers needed.
-            W, b = tf.get_variable('W'), tf.get_variable('b')
-        return W, b
-
-    def _setup_predictions(self, Y):
+    def _setup_training(self, batch_size):
         """Unfolds the network for a batch of truncated sequences ("in
         parallel") until the output layer using the precomputed hidden states.
 
@@ -156,19 +109,72 @@ class MultilayerGRU(object):
             Y: A tensor with shape [batch_size, self.unfolding_depth].
                 Represents all output labels organized in batches.
         """
-        batch_size, truncate_at = Y.get_shape()
-        W, b = self._get_model_params(self.h_to_o_scope)
-        logits = [tf.matmul(hid, W) + b for hid in self.hidden]
-        # Predictions (not used here directly).
-        _ = [tf.nn.softmax(logit) for logit in logits]
-        # Turn our y placeholder into a list of labels.
+        # Turn our Y placeholder into a list of labels.
+        depth = self.unfolding_depth
+        self.y = tf.placeholder(tf.int32, [batch_size, depth])
         y_as_list = [tf.squeeze(i, squeeze_dims=[1])
-                     for i in tf.split(1, self.unfolding_depth, Y)]
-        loss_weights = [tf.ones([batch_size]) for i in range(truncate_at)]
+                     for i in tf.split(1, depth, self.y)]
+        loss_weights = [tf.ones([batch_size]) for i in range(depth)]
         losses = tf.nn.seq2seq.sequence_loss_by_example(
-            logits, y_as_list, loss_weights)
+            self._logits, y_as_list, loss_weights)
         train_step = tf.train.AdagradOptimizer(self.eta).minimize(losses)
         return train_step, losses
+
+    def _train(self, sess, train_data):
+        _train_step, _step_loss = self._setup_training(train_data.batch_size)
+        sess.run(tf.initialize_all_variables())
+        for epoch_num, epoch in enumerate(train_data):
+            step_loss = 0.
+            # `final_hidden` is the evaluated final hidden state for
+            # current batch. We tell tensorflow to use it as the
+            # "initial" (t_{-1}) state for the next batch by placing
+            # it in the `feed_dict` kwarg. Note that `self._initial_hidden`
+            # is a tensorflow (unevaluated) tensorflow object that is
+            # used in `self._setup_input_processing`.
+            final_hidden = None
+            for X, Y in epoch:
+                # `step_loss` is the mean loss for current step. Note that
+                # we pass `_step_loss` to `sess.run` just because we want
+                # to print something after each epoch. The only value that
+                # we really need to evaluate (and reuse) for training is
+                # `_final_hidden`.
+                feed_dict = {self.x: X, self.y: Y}
+                # First step already has valid (zero) initial hidden state.
+                # For further steps we need to use final hidden state for
+                # current batch as our new initial hidden state.
+                #
+                # `is not None` here is not bad style; `final_hidden` is
+                # going to become an array an will raise a `ValueError` if
+                # being used as a truth value.
+                if final_hidden is not None:
+                    feed_dict[self._initial_hidden] = final_hidden
+                step_loss, final_hidden, _ = sess.run(
+                    [_step_loss, self._final_hidden, _train_step],
+                    feed_dict=feed_dict)
+            logging.info("Epoch %s, mean loss: %s",
+                         epoch_num, np.mean(step_loss))
+            if np.mean(step_loss) <= 0.42:
+                logging.info("Model successfully trained")
+                return
+
+    def _predict(self, sess, data):
+        final_hidden = None
+        for X, Y in data:
+            feed_dict = {self.x: X}
+            # First step already has valid (zero) initial hidden state.
+            # For further steps we need to use final hidden state for
+            # current batch as our new initial hidden state.
+            #
+            # `is not None` here is not bad style; `final_hidden` is
+            # going to become an array an will raise a `ValueError` if
+            # being used as a truth value.
+            if final_hidden is not None:
+                feed_dict[self._initial_hidden] = final_hidden
+            prediction, final_hidden = sess.run(
+                [self._logits, self._final_hidden], feed_dict=feed_dict)
+            logging.info(
+                "Predicted: %s\tExpected:%s",
+                np.argmax(prediction, axis=2).T[0], Y[0])
 
 
 if __name__ == "__main__":
